a095c3d35ffbdc7e71f7b03afe5d2b1f3fda8bfc
douglasmcl <douglasmacielaraujo@outlook.com>
2024-04-01 00:11:04 -0300

    first ex

--- aulas/src/application/aula1.java
+++ aulas/src/application/aula1.java
@@ -5,6 +5,16 @@ public class aula1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println(factorial(5));
 	}
-
-}
+	
+	public static int factorial(Integer num) {
+		int acum = 1;
+		
+		for(int i = 1; i<=num; i++) {
+			acum = acum * i;
+		}
+		
+		return acum;
+	}
+} 
