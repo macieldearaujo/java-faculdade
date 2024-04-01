89d3482b23e2093707c353b411f4e9cd51f19641
douglasmcl <douglasmacielaraujo@outlook.com>
2024-04-01 00:54:00 -0300

    aula03 fibonnaci

---
+++ aulas/src/application/Aula03.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+public class Aula03 {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite quantos termos da sequência de Fibonnaci você quer mostrar: ");
+		int times = sc.nextInt();
+		
+		executeFibonacci(times);
+		
+		sc.close();
+	}
+	
+	public static void executeFibonacci(Integer times) {
+		int values[] = new int[times];
+		values[0] = 1;
+		values[1] = 1;
+		
+		for(int i = 2; i < times; i++) {
+			values[i] = values[i-1] + values[i-2];
+		}
+		
+		for(int i = 0; i<values.length; i++) {
+			System.out.print(values[i] + ", ");
+		}
+		
+	}
+}
